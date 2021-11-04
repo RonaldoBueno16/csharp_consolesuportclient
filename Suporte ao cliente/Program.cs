1146026a679bd7e7581ee20415545c6dd9068fb9
RonaldoBueno16 <ronaldobueno2002@gmail.com>
2021-11-04 20:20:36 -0300

    Adicionando menu

--- Suporte ao cliente/Program.cs
+++ Suporte ao cliente/Program.cs
@@ -6,7 +6,43 @@ namespace Suporte_ao_cliente
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int opcao;
+            
+            while(true)
+            {
+                Console.WriteLine("=====================================");
+                Console.WriteLine("====[Selecione a opção desejada]=====");
+                Console.WriteLine("=====================================");
+                Console.WriteLine("1 - Entrar como cliente");
+                Console.WriteLine("2 - Entrar como suporte");
+                Console.WriteLine("3 - Encerrar");
+
+                Voltar:
+                Console.Write("\nOpção desejada: ");
+                string teste = Console.ReadLine();
+                if (teste == "" || (teste != "1" && teste != "2" && teste != "3"))
+                {
+                    Console.WriteLine("Você precisa digitar uma opção válida!");
+                    goto Voltar;
+                }
+                opcao = int.Parse(teste);
+
+                if(opcao == 1)
+                {
+                    Console.WriteLine("Você entrou na área do cliente");
+                }
+                else if(opcao == 2)
+                {
+                    Console.WriteLine("Você entrou na área do suporte");
+                }
+                else if(opcao == 3)
+                {
+                    Console.WriteLine("Você fechou o programa!\n\n");
+                    break;
+                }
+            }
         }
     }
+
+    
 }
